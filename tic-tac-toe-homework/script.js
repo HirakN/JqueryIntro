cdcc5ee2efe29addedb06bd35e885f5152f9ba20
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2017-12-14 21:36:41 +0000

    added variables to store elements for homeowrk

--- tic-tac-toe-homework/script.js
+++ tic-tac-toe-homework/script.js
@@ -1,21 +1,36 @@
-// Two ways of checking if DOM is ready
-$(function(event) {
-	console.log("DOM is ready");
-});
+// // Two ways of checking if DOM is ready
+// $(function(event) {
+// 	console.log("DOM is ready");
+// });
+
+// $(document).ready( function() {
+// 	console.log("ready!");
+// });
+
+// $(document).ready(
+// 	// All jquery in here
+// );
+
+// $(document).ready(function(){
+// 	$("#reset").click(function(){
+// 		$("h2").hide();
+// 	})
+// 	$("#show").click(function(){
+// 		$("h2").show();
+// 	})
+// })
+
+// 1. Main function with logic for knowing which player turn it is.
+// 2. Attach event listeners to each <td> element.
+// 3. On player X/O turn any clicks on boxes will
+
+
+var reset = $
 
 $(document).ready( function() {
-	console.log("ready!");
+	var reset = $("#reset");
+	var boxes = $("td")
+	var turn
+
+	console.log(reset);
 });
-
-$(document).ready(
-	// All jquery in here
-);
-
-$(document).ready(function(){
-	$("#reset").click(function(){
-		$("h2").hide();
-	})
-	$("#show").click(function(){
-		$("h2").show();
-	})
-})
